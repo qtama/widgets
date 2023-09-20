75ecc1d6b55bf7e143bdfe8b6d212054bcacc29e
Тамаров Алексей Геннадьевич <a.tamarov@rit.va>
2023-09-20 13:52:08 +0000

    canonical and custom (vanity) routes

---
+++ app/controllers/widgets_controller.rb
@@ -0,0 +1,16 @@
+class WidgetsController < ApplicationController
+  def index
+    @widgets = [
+      OpenStruct.new(id: 1, name: 'Stembolt'),
+      OpenStruct.new(id: 2, name: 'Flux Capacitor')
+    ]
+  end
+
+  def show
+    @widget = OpenStruct.new(
+      id: params[:id],
+      manufacturer_id: rand(100),
+      name: "Widget #{params[:id]}"
+    )
+  end
+end
