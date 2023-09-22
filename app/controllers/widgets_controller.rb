da0aff2c5dc9ab5f42a350d405f1fc173ae22ab4
Тамаров Алексей Геннадьевич <a.tamarov@rit.va>
2023-09-22 11:08:41 +0000

    views, partials, helpers, presenters, css

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -7,10 +7,34 @@ class WidgetsController < ApplicationController
   end
 
   def show
+    manufacturer = OpenStruct.new(
+      id: rand(100),
+      name: 'Sector 7G',
+      address: OpenStruct.new(
+        id: rand(100),
+        country: 'UK'
+      )
+    )
+
     @widget = OpenStruct.new(
       id: params[:id],
-      manufacturer_id: rand(100),
+      manufacturer_id: manufacturer,
+      manufacturer: manufacturer,
+      # manufacturer_name: "Sector 7G",
+      # manufacturer_country: "UK",
       name: "Widget #{params[:id]}"
     )
+
+    @widget.instance_eval do
+      def widget_id
+        if id.to_s.length < 3
+          id.to_s
+        else
+          "#{id.to_s[..-3]}.#{id.to_s[-2..]}"
+        end
+      end
+    end
+
+    # @widget_presenter = WidgetPresenter.new(widget)
   end
 end
